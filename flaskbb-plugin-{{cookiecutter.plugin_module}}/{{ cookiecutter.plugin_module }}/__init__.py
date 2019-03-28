2ee3cfd1abd3aa4b81d38474f95961cccedc4c0b
Peter Justin <peter.justin@outlook.com>
2019-03-28 20:35:26 +0100

    Explicitly mark hooks using the HookimplMarker
    Implicit hook loading is deprecated by pluggy

--- flaskbb-plugin-{{cookiecutter.plugin_module}}/{{ cookiecutter.plugin_module }}/__init__.py
+++ flaskbb-plugin-{{cookiecutter.plugin_module}}/{{ cookiecutter.plugin_module }}/__init__.py
@@ -9,29 +9,38 @@
     :license: {{ cookiecutter.license }}, see LICENSE for more details.
 """
 import os
+
+from pluggy import HookimplMarker
+
 from flaskbb.forum.models import Forum
-from flaskbb.utils.helpers import render_template
 from flaskbb.utils.forms import SettingValueType
+from flaskbb.utils.helpers import render_template
 
 from .views import {{ cookiecutter.plugin_module + "_bp" }}
-
 
 __version__ = "{{ cookiecutter.version }}"
 
 
+hookimpl = HookimplMarker("flaskbb")
+
+
 # connect the hooks
+@hookimpl
 def flaskbb_load_migrations():
     return os.path.join(os.path.dirname(__file__), "migrations")
 
 
+@hookimpl
 def flaskbb_load_translations():
     return os.path.join(os.path.dirname(__file__), "translations")
 
 
+@hookimpl
 def flaskbb_load_blueprints(app):
     app.register_blueprint({{ cookiecutter.plugin_module + "_bp" }}, url_prefix="/{{ cookiecutter.plugin_module }}")
 
 
+@hookimpl
 def flaskbb_tpl_before_navigation():
     return render_template("{{ cookiecutter.plugin_module }}_navlink.html")
 
